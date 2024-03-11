33e2c1a0fa61465bc959ceaa55c2cb23d055682e
meet1608 <meetvisodiya3@gmail.com>
2024-03-11 21:31:54 +0530

    fourth commit

--- food-ordering-app/food-ordering-front/src/services/MealTypeService.js
+++ food-ordering-app/food-ordering-front/src/services/MealTypeService.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import TokenService from "./TokenService";
 
 class MealTypeService{
-    const a;
     getAllMealTypes(){
         return axios.get("http://localhost:8088/api/mealType/getAllMealTypes");
     }
